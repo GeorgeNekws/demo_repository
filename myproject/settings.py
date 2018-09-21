6ff878450694e500e0e3fb2984f9f942ce7ff407
GeorgeNekws <gtsikriteas@uth.gr>
2018-09-21 12:03:32 +0200

    init

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hc$h%wayj_aq#2!7&av*jqu1cg9xdo4q!c-5y)73645e-p$(a9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost'. 'https://cryptic-hollows-63700.herokuapp.com/']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sample_database',
+        'USER':'sample_user',
+        'PASSWORD': 'sample_password',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
